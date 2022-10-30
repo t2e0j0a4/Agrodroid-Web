189f7dbe7474aac43344a1815a3060c6d933067b
Nagateja <teja91.valiveti@gmail.com>
2022-10-30 22:38:12 +0530

    Project - About Pages Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}","./public/*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        leaguespartan: ["League Spartan", "serif"],
+        nunitosans: ["Nunito Sans" , "serif"],
+      },
+    },
   },
   plugins: [],
 }
