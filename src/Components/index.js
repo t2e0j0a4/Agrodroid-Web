4d9b701527283aac4e9128758cf215ed2f92f7e0
Nagateja <teja91.valiveti@gmail.com>
2022-11-20 21:30:46 +0530

    Components Exports Optimized

---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+export {default as About} from "./About";
+export {default as Achivements} from "./Achivements";
+export {default as Advantages} from "./Advantages";
+export {default as Events} from "./Events";
+export {default as FAQ} from "./FAQ";
+export {default as Footer} from "./Footer";
+export {default as Home} from "./Home";
+export {default as Partners} from "./Partners";
+export {default as PhotoGallery} from "./PhotoGallery";
+export {default as Services} from "./Services";
