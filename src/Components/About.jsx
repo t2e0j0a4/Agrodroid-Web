5621ca1a0770e4c3e306e3a2282efb1013dbbf90
Nagateja <teja91.valiveti@gmail.com>
2023-01-23 20:48:59 +0530

    Mentor Details Added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,13 +7,12 @@ import Binoculars from "../Images/Binoculars.png";
 import Target from "../Images/Target.png";
 import Team from "../Images/Team.png";
 
-
-
 import Partner1 from "../Images/Partner-1.png";
 import Partner2 from "../Images/Partner-2.png";
 import Partner3 from "../Images/Partner-4.png";
 import Partner4 from "../Images/Partner-3.png";
 
+import manojkumar from "../Images/Manoj Kumar Mentor.svg";
 
 const teamMembers = [
     {key:1, name:"Mahesh Vuppu" , linkedin: "https://linkedin.com/in/maheshvuppu" , instagram:"https://instagram.com/iam_mahesh___", desc:"Extremely ambitious to accomplish great things through serial entrepreneurship." , role:"Co-Founder" , animation:"zoom-in" , duration:800},
@@ -21,9 +20,6 @@ const teamMembers = [
     {key:3, name:"Naveen Gadala" , linkedin: "https://linkedin.com/in/naveengadala-075757254" , instagram:"https://instagram.com/navii._.07/", desc:"He conducts effective market and consumer surveys and has outstanding interpersonal skills." , role:"Co-Founder" , animation:"zoom-in" , duration:800},
     {key:4, name:"Jeshwanth Reddy.K" , linkedin: "https://www.linkedin.com/in/k-jeswanth-reddy-b778b8245" , instagram:"https://instagram.com/jeshwanth_reddyyy", desc:"Enthusiastic, a person who is really passionate, enjoys working with a team, and is punctual" , role:"Co-Founder" , animation:"zoom-out" , duration:600}
 ]
-
-
-
 
 const About = () => {
   return (
@@ -78,13 +74,13 @@ const About = () => {
               <div className="inner w-[90%] md:w-[50%] h-[auto] flex flex-col items-center justify-center">
                     <img  loading='lazy' src={Binoculars} alt="Vision" className="w-[40%]" data-aos="zoom-out" data-aos-duration="800"/>
                     <div className="inner-head text-lg font-bold my-6 italic text-center font-leaguespartan">Vision</div>
-                    <div className="sub-head-line text-md w-[70%] text-center font-semibold">Enlight the technology in farming to make it independent of labour.</div>
+                    <div className="sub-head-line text-md w-[90%] sm:w-[70%] text-center font-semibold">Enlight the technology in farming to make it independent of labour.</div>
               </div>
               <div className="line w-1 h-96 bg-[#252525] rounded hidden md:block"></div>
               <div className="inner w-[85%] md:w-[50%] h-[auto] flex flex-col items-center justify-center">
                     <img  loading='lazy' src={Target} alt="Mission" className="w-[60%] md:w-[50%] lg:w-[40%] " data-aos="zoom-out" data-aos-duration="800"/>
                     <div className="inner-head text-lg font-bold my-6 italic text-center font-leaguespartan">Mission</div>
-                    <div className="sub-head-line text-md w-[70%] text-center font-semibold">To offer technical solutions for smart agriculture as a service to grow up its Production quality and revenues for farmers with 0% mental stress.</div>
+                    <div className="sub-head-line text-md w-[90%] sm:w-[70%] text-center font-semibold">To offer technical solutions for smart agriculture as a service to grow up its Production quality and revenues for farmers with 0% mental stress.</div>
               </div>
             </div>
 
@@ -118,6 +114,23 @@ const About = () => {
               })
             }
           </div>
+
+          <div className="w-[100%] h-[100%] flex flex-col items-center justify-center my-3 gap-y-4 mt-5">
+            <div className="flex flex-col w-[100%] items-center justify-center">
+              <div className="main-head-v-m text-2xl font-nunitosans font-semibold text-[#004346] text-center" data-aos="zoom-in" data-aos-duration="400">Product Mentor</div>
+              <div data-aos="zoom-in" data-aos-duration="700" className="w-28 h-[0.15rem] bg-[#f29f05] rounded text-center m-auto mb-4 mt-2"></div>
+            </div>
+            <div className="flex items-center justify-center flex-col w-[280px] sm:w-[300px] gap-y-2 bg-white py-5 px-3 rounded-[8px] hover:shadow-md hover:scale-105 transition-all duration-700" data-aos="zoom-out" data-aos-duration="900" data-aos-offset="250">
+              <img src={manojkumar} alt="Product Mentor" className='w-[80%] object-cover rounded-[50%]'/>
+              <span className='font-leaguespartan w-[96%] mx-auto text-center text-[16px]'>Manoj Kumar Bada Ghar Wala</span>
+              <span className='font-[Poppins] text-[14px] italic'>Product Development Mentor</span>
+              <div className="flex flex-col items-center justify-center text-center">
+                <span className='font-[Poppins] text-[14px]'>Director, Badagharwala Technologies</span>
+                <span className='font-[Poppins] text-[14px]'>Mentor at T-hub, ALEAP WE HUB, BVIC, AIM, CIES, UNIDO</span>
+              </div>
+            </div>
+          </div>  
+
           <div className="my-7 mx-auto w-[100%] flex flex-col justify-center items-center space-y-5">
             <div className="text-center font-bold text-xl italic">and</div>
             <div className="text-center font-medium text-xl w-[90%] mx-auto md:w-auto">We have a Passionate technical team of <span className="font-bold text-2xl text-orange-500">15+ members</span> who are working on this project.</div>
